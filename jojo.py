aff7940f3dfcb86fd6f3fba3e467ab95fdd53be6
renee <jupiterr.chen@gmail.com>
2015-02-28 00:19:49 +0800

    first commit

---
+++ jojo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+"""
+author : Giraffe Chen
+date : 2015/02/27
+
+"""
+
+
+def jojo(instr):
+	if instr == "hah":
+		return "yes"
+	else:
+		return "no"
+
+print jojo("hah")
